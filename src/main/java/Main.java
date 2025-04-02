a6cb1f4d9333812073b537396a97113667c402fb
jakubek16cm <kuba.stanczyk201@gmail.com>
2025-04-02 14:01:21 +0000

    3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,21 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int n= 5; 
+   
+    for(int i= n; i > 0; i--){
+      
+  
+      for(int j=0; j < n-i; j++)
+      {
+        System.out.print(" ");
+      }
+      for(int k=0; k < i; k++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+  }
   }
 
   // @Test
